0be8c586645d44c19170dbccb5ead84c3fab5437
Enzo Simonin <nzosim54@gmail.com>
2026-01-23 16:13:57 +0100

    init dark and light mode color

---
+++ front_back/app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-btn @click="theme.toggle()" text="Toggle Light / Dark"></v-btn>
+
+    Test
+    <v-btn color="primary">Valider</v-btn>
+</template>
+
+<script setup>
+    import { useTheme } from 'vuetify';
+    const theme = useTheme();
+</script>
---
+++ front_back/plugins/vuetify.js
@@ -0,0 +1,36 @@
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { md3 } from 'vuetify/blueprints';
+import 'vuetify/styles';
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const vuetify = createVuetify({
+        blueprint: md3,
+        components,
+        directives,
+        theme: {
+            defaultTheme: 'system',
+            themes: {
+                light: {
+                    colors: {
+                        primary: '#1976D2',
+                        secondary: '#424242',
+                        accent: '#82B1FF',
+                        error: '#FF5252',
+                        info: '#2196F3',
+                        success: '#4CAF50',
+                        warning: '#FB8C00',
+                    },
+                },
+                dark: {
+                    colors: {
+                        primary: '#474a4d',
+                    },
+                },
+            },
+        },
+    });
+
+    nuxtApp.vueApp.use(vuetify);
+});
