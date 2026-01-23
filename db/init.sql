d019990be3f8d9a21d6fd3e00749e53ebd69bbb2
Enzo Simonin <nzosim54@gmail.com>
2026-01-23 14:31:52 +0100

    init folder and docker compose

---
+++ db/init.sql
@@ -0,0 +1,18 @@
+-- Adminer 5.2.1 MySQL 8.0.45 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `Test`;
+CREATE TABLE `Test` (
+  `id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `Test` (`id`) VALUES
+(1);
+
+-- 2026-01-23 13:28:23 UTC
