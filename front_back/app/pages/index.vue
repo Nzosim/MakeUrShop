6ca29c24279ec9a4a3a410d6d5042a6a16e74ec5
Joan RODRIGUES <rodrig376u@etu.univ-lorraine.fr>
2026-02-02 15:59:15 +0100

    add navbar and related modifications

--- front_back/app/pages/index.vue
+++ front_back/app/pages/index.vue
@@ -1,15 +1,4 @@
 <template>
-    <v-btn icon @click="theme.toggle()">
-        <v-icon>
-            {{ theme.global.name.value === 'light' ? 'mdi-weather-night' : 'mdi-white-balance-sunny' }}
-        </v-icon>
-    </v-btn>
-
     Test
     <v-btn color="primary" to="/test/test">Go test page</v-btn>
 </template>
-
-<script setup>
-    import { useTheme } from 'vuetify';
-    const theme = useTheme();
-</script>
