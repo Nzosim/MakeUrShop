4a0a8618dafc060a8050bddae92f08c1b40dfbbc
joanRds <joan.rodrigues6@etu.univ-lorraine.fr>
2026-02-04 15:43:31 +0100

    add footer & correction config.json

--- front_back/app/app.vue
+++ front_back/app/app.vue
@@ -5,9 +5,11 @@
             <slot />
             <NuxtPage />
         </v-main>
+        <Footer />
     </v-app>
 </template>
 
 <script setup>
     import Navbar from './components/Main/Navbar.vue';
+    import Footer from './components/Main/Footer.vue';
 </script>
---
+++ front_back/app/components/Main/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-footer class="d-flex align-center justify-center ga-2 flex-wrap flex-grow-1 py-3 mt-10" color="surface" elevation="8">
+        <v-btn v-for="link in links" :key="link" :text="link" variant="text" rounded class="categories"></v-btn>
+
+        <div class="flex-1-0-100 text-center mt-2">
+            {{ new Date().getFullYear() }} —
+            <strong>{{ storeName }}</strong>
+            — {{ storeEmail }}
+        </div>
+    </v-footer>
+</template>
+<script setup>
+    import configData from '@/assets/config.json';
+    const storeName = configData.config.info.storeName;
+    const storeEmail = configData.config.info.storeEmail;
+    const links = ['CGV', 'MENTIONS LÉGALES', 'POLITIQUE DE CONFIDENTIALITÉ', 'FAQ', 'NOUS CONTACTER'];
+</script>
