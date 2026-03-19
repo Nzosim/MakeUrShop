afe41720ae6ab35734d0a93274b288bd1f8c0f2f
Timothe Farina <Timothe.farina1@etu.univ-lorraine.fr>
2026-03-19 10:50:26 +0100

    docker-composer update (windows refresh) + button

--- front_back/app/pages/Admin/Stock.vue
+++ front_back/app/pages/Admin/Stock.vue
@@ -1,13 +1,20 @@
 <template>
     <h1>Stock disponible</h1>
 
+    <ButtonsAddStock />
+
+
+    
     <stock-list :products="data" />
+
+    
 
 </template>
 
 
 <script setup>
     import StockList from '~/components/Admin/StockList.vue';
+    import ButtonsAddStock from '~/components/Admin/ButtonsAddStock.vue';
 
     const { data } = await useFetch('/api/stock/getStock');
 </script>
