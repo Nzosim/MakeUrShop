4d35258977a9267cde1b4deea622ac105f247d1a
Timothe Farina <Timothe.farina1@etu.univ-lorraine.fr>
2026-03-20 10:41:02 +0100

    update /Article List

---
+++ front_back/app/pages/Admin/ArticleList.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Articles</h1>
+
+    <ButtonsAddStock />
+    <ProductListAdmin />
+</template>
+
+<script setup>
+    // import StockList from '~/components/Admin/StockList.vue';
+    import ButtonsAddStock from '~/components/Admin/ButtonsAddStock.vue';
+    import ProductListAdmin from '~/components/Admin/ProductListAdmin.vue';
+
+    const { data } = await useFetch('/api/stock/getProducts');
+</script>
