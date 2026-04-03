db318ea7e35e8d8cc155ff155dcb120eaa8d45b9
Timothe Farina <timofarina45130@gmail.com>
2026-04-03 08:45:39 +0200

    add addBrand.js

--- front_back/server/api/admin/addBrand.js
+++ front_back/server/api/admin/addBrand.js
@@ -2,12 +2,25 @@ import { getDB } from '../../utils/db.js';
 
 export default defineEventHandler(async (event) => {
     const db = getDB();
+    const { name } = await readBody(event);
 
-    const request = `
-        INSERT INTO brand (id, name) VALUES (DEFAULT, ?)
+    const checkRequest = `
+        SELECT id, name FROM brand WHERE name = ?
     `;
 
-    const [rows] = await db.query(request, [brandName]);
+    const [existingBrand] = await db.query(checkRequest, [name]);
+
+    if (existingBrand.length) {
+        throw createError({
+            statusCode: 409,
+            message: `La marque "${existingBrand[0].name}" existe déjà.`,
+        });
+    }
+    const request = `
+        INSERT INTO brand (name) VALUES (?)
+    `;
+
+    const [rows] = await db.query(request, [name]);
 
     console.log(rows);
     return rows;
