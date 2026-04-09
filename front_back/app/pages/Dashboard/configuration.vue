c64c229ee94aa293744c83f16492a24970cdf4fa
joanRds <joan.rodrigues6@etu.univ-lorraine.fr>
2026-04-09 15:25:07 +0200

    Add Configuration page for administrators

---
+++ front_back/app/pages/Dashboard/configuration.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-container>
+        <TextEditor />
+    </v-container>
+</template>
+
+<script setup>
+    import TextEditor from '@/components/Main/TextEditorGlobal.vue';
+    definePageMeta({
+        layout: 'admin',
+    });
+</script>
