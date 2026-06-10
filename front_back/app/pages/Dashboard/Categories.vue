b00611aa7bbaf23ca57c514546dc9eb2a8199af3
Timothe Farina <timofarina45130@gmail.com>
2026-06-10 10:40:42 +0200

    add page Categories

---
+++ front_back/app/pages/Dashboard/Categories.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card class="mt-5 ml-5 mr-5 pa-6" rounded="lg">
+        <Category />
+    </v-card>
+</template>
+
+<script setup>
+    definePageMeta({
+        layout: 'admin',
+    });
+
+    import Category from '~/components/Admin/Category.vue';
+</script>
