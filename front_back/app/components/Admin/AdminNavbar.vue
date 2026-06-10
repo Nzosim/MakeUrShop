70c640fb72385dd10fe155ce6f428dc6130a489f
Enzo Simonin <nzosim54@gmail.com>
2026-06-10 14:38:32 +0200

    change from href for to

--- front_back/app/components/Admin/AdminNavbar.vue
+++ front_back/app/components/Admin/AdminNavbar.vue
@@ -2,31 +2,33 @@
     <v-navigation-drawer permanent>
         <v-list>
             <v-list-item class="d-flex justify-center">
-                <div class="logo-text">{{ storeName }}</div>
+                <div class="logo-text">Dashboard</div>
             </v-list-item>
         </v-list>
+
         <v-list density="compact" nav active-color="primary">
-            <v-list-item prepend-icon="mdi-poll" title="Statistiques" value="starred" href="/dashboard/stats"></v-list-item>
-            <v-list-item prepend-icon="mdi-cart" title="Commandes" value="prout2" href="/dashboard/order"></v-list-item>
+            <v-list-item prepend-icon="mdi-poll" title="Statistiques" value="stats" to="/dashboard/stats"></v-list-item>
+            <v-list-item prepend-icon="mdi-cart" title="Commandes" value="orders" to="/dashboard/order"></v-list-item>
+
             <v-list-group value="gestion">
                 <template #activator="{ props }">
                     <v-list-item v-bind="props" prepend-icon="mdi-tag" title="Gestion"></v-list-item>
                 </template>
 
-                <v-list-item prepend-icon="mdi-package-variant" title="Stockage" value="articles" href="/dashboard/stockage"></v-list-item>
-                <v-list-item prepend-icon="mdi-tag" title="Marques" value="marques" href="/dashboard/marque"></v-list-item>
-                <v-list-item prepend-icon="mdi-shape" title="Catégories" value="categories" href="/dashboard/categories"></v-list-item>
+                <v-list-item prepend-icon="mdi-package-variant" title="Stockage" value="stockage" to="/dashboard/stockage"></v-list-item>
+                <v-list-item prepend-icon="mdi-tag" title="Marques" value="marques" to="/dashboard/marque"></v-list-item>
+                <v-list-item prepend-icon="mdi-shape" title="Catégories" value="categories" to="/dashboard/categories"></v-list-item>
             </v-list-group>
 
-            <v-list-item prepend-icon="mdi-account-group" title="Clients" value="shared" href="/dashboard/accounts"></v-list-item>
-            <v-list-item prepend-icon="mdi-text-shadow" title="Configuration" value="starred" href="/dashboard/configuration"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-group" title="Clients" value="clients" to="/dashboard/accounts"></v-list-item>
+            <v-list-item prepend-icon="mdi-text-shadow" title="Configuration" value="config" to="/dashboard/configuration"></v-list-item>
         </v-list>
 
         <v-list style="position: absolute; bottom: 0px; width: 100%">
             <v-divider class="mb-2"></v-divider>
             <v-list-item title="Administrateur">
                 <template #append>
-                    <v-btn icon="mdi-home" size="small" variant="text" href="/" />
+                    <v-btn icon="mdi-home" size="small" variant="text" to="/" />
                 </template>
             </v-list-item>
         </v-list>
@@ -45,8 +47,3 @@
         letter-spacing: 1px;
     }
 </style>
-
-<script setup>
-    import configData from '@/assets/config.json';
-    const storeName = configData.config.info.storeName;
-</script>
