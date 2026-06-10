525fd3bf3ba3fbbb1b781ac53287cea82f83f46e
Timothe Farina <timofarina45130@gmail.com>
2026-06-10 10:40:57 +0200

    add page Marque

---
+++ front_back/app/pages/Dashboard/Marque.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card class="mt-5 ml-5 mr-5 pa-6" rounded="lg">
+        <Brand />
+    </v-card>
+</template>
+
+<script setup>
+    definePageMeta({
+        layout: 'admin',
+    });
+
+    import Brand from '~/components/Admin/Brand.vue';
+</script>
